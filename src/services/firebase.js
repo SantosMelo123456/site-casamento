74bd722baf04a1c2c38a451feab330be98bc6f2e
SantosMelo123456 <lindamariasmelo@gmail.com>
2026-03-13 15:26:44 -0300

    Sprint 1: setup inicial React + Firebase

---
+++ src/services/firebase.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDvCYHkVTh2tjffZhesqZ5yux5JDdprA80",
+  authDomain: "site-casamento-842ed.firebaseapp.com",
+  projectId: "site-casamento-842ed",
+  storageBucket: "site-casamento-842ed.firebasestorage.app",
+  messagingSenderId: "1091352765017",
+  appId: "1:1091352765017:web:820bd4461d7f4f6cc38748"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export default app;
